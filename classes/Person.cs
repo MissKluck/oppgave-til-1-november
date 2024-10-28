f27d18c7fd18f1f2452c8357dbbe5b8acb092439
MissKluck <sigrid.b.kluck@gmail.com>
2024-10-28 14:44:16 +0100

    added a person class

---
+++ classes/Person.cs
@@ -0,0 +1,8 @@
+public class Person
+{
+    public string? Name { get; set; }
+    public int? Age { get; set; }
+    public string? Place { get; set; }
+    public string? Position { get; set; }
+
+}
